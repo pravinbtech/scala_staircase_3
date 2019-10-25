b6d82b59968b205436090a1c223ced37497f2013
pravinbtech <pravinbtech@gmail.com>
2019-10-25 08:51:42 -0400

    Advanced scala

--- src/main/java/com/github/advancedscala/collecttut/Set.scala
+++ src/main/java/com/github/advancedscala/collecttut/Set.scala
@@ -15,6 +15,7 @@ trait MySet[A] extends (A=> Boolean) {
   def -(elem:A):MySet[A] //removing
   def &(anotherset:MySet[A]):MySet[A] //Intersection
   def --(anotherset:MySet[A]):MySet[A] //difference
+  def unary_! :MySet[A]
 }
 
 class EmptySet[A] extends MySet[A]{
@@ -28,6 +29,8 @@ class EmptySet[A] extends MySet[A]{
   def -(elem:A):MySet[A]=this
   def &(anotherset:MySet[A]):MySet[A]=this
   def --(anotherset:MySet[A]):MySet[A]=this
+  //def unary_! :MySet[A]=this
+  def unary_! : MySet[A]=new PropertybasedSet[A](_=>true)
 }
 
 class NonemptySet[A] (head:A,tail:MySet[A])extends MySet[A]{
@@ -41,7 +44,7 @@ if (this contains elem)
   new NonemptySet[A](elem,this)
   }
 
-//  List(1,2,3) ++ aList(4,5)
+//  List(1,2,3) ++ List(4,5)
 //
 //  List(2,3)++List(4,5)+1
 //  List(3)++List(4,5)+1+2
@@ -50,7 +53,7 @@ if (this contains elem)
     tail ++ anotherSet + head
   }
   def map[B](f:A=>B):MySet[B]={
-    (tail map f)+f(head)
+    (tail.map(f) +f(head))
   }
   def foreach(f:A=>Unit):Unit={
     tail foreach f
@@ -75,14 +78,51 @@ if (this contains elem)
   }
 
   def &(anotherset:MySet[A]):MySet[A]={
-  filter(x=>anotherset.contains(x))
+  //filter(x=>anotherset.contains(x))
+   // filter(x=>anotherset(x))
+    filter(anotherset)
   }
 
   def --(anotherset:MySet[A]):MySet[A]={
+//filter(x => !anotherset.contains(x))
 
+    filter (x=> !anotherset(x))
   }
 
+  def unary_! : MySet[A]=new PropertybasedSet[A](x=> !this.contains(x))
 }
+
+
+class PropertybasedSet[A] (property:A=>Boolean) extends MySet[A] {
+
+  def contains(elem:A):Boolean=property(elem)
+
+//  def contains(elem: A): Boolean = property(elem)
+  // { x in A | property(x) } + element = { x in A | property(x) || x == element }
+  def +(elem: A): MySet[A] =
+    new PropertybasedSet[A](x=>property(x)|| x==elem)
+  //  new PropertybasedSet[A](x => property(x) || x == elem)
+
+  // { x in A | property(x) } ++ set => { x in A | property(x) || set contains x }
+  def ++(anotherSet: MySet[A]): MySet[A] =
+    new PropertybasedSet[A](x => property(x) || anotherSet(x))
+
+  // all integers => (_ % 3) => [0 1 2]
+  def map[B](f: A => B): MySet[B] = politelyFail
+  def flatmap[B](f: A => MySet[B]): MySet[B] = politelyFail
+  def foreach(f: A => Unit): Unit = politelyFail
+
+  def filter(predicate: A => Boolean): MySet[A] = new PropertybasedSet[A](x => property(x) && predicate(x))
+  def -(elem: A): MySet[A] = filter(x => x != elem)
+  def --(anotherSet: MySet[A]): MySet[A] = filter(!anotherSet)
+  def &(anotherSet: MySet[A]): MySet[A] = filter(anotherSet)
+  def unary_! : MySet[A] = new PropertybasedSet[A](_=>false)
+
+  def politelyFail = throw new IllegalArgumentException("Really deep rabbit hole!")
+}
+
+
+
 
 object MySet{
 
@@ -103,14 +143,27 @@ object MySet{
   }
 }
 object MySetPlayground extends App {
-  val s = MySet(1, 2, 3, 4)
-  s.foreach(println(_))
+  val s = MySet(1, 2, 3, 4,5)
   s + 5 ++ MySet(-1, -2) + 3 foreach (println(_))
   //flatmap (x => MySet(x, 10 * x)) filter (_ % 2 == 0) foreach println
-//s.foreach(println(_))
-val d =MySet("a","b","c")
+  s.foreach(println(_))
+  val d = MySet("a", "b", "c")
 
- val g= d+"e"
-g.foreach(println(_))
+  // val g= d+"e"
+  //g.foreach(println(_))
+  val negative = !s ++ MySet(8,10,12)
+
+  //x.foreach(println(_))
+  println(negative(2))
+  println(negative(9))
+
+  val negativeEven = negative.filter(_ % 2 == 0)
+  println(negativeEven(10))
+ println(negativeEven(20))
+  val negativeEven5 = negativeEven + 10 // all the even numbers > 4 + 5
+  println(negativeEven5(14))
+
+  val pri: Boolean =MySet(1,2,3) contains(5)
+  println(pri)
 
 }
